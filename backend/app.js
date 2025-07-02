a796d9f7e1f3a1fa1060089a89e4e3a8a0f48d95
BiancaGabrielaA <biancagabriela.asavoaei@gmail.com>
2025-07-02 22:30:01 +0300

    Setting up project and backend

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express()
+
+dotenv.config({path: 'backend/config/config.env'});
+
+///connecting to database
+import { connectDatabase } from "./config/dbConnect.js";
+connectDatabase();
+
+///import all routes
+
+import productRoutes from './routes/products.js'
+
+app.use("/api/v1", productRoutes);
+
+app.listen(3000, () => {
+    console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode `);
+});
