19b256ce77d2a6ea8fd7e7115702b52b1ef15b60
BiancaGabrielaA <biancagabriela.asavoaei@gmail.com>
2025-07-03 18:44:21 +0300

    jwt token

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -3,11 +3,14 @@ import User from "../models/user.js";
 
 export const registerUser = catchAsyncErrors( async(req, res, next) => {
     const { name, email, password } = req.body;
+    
     const user = await User.create({
         name, email, password
     })
 
-    res.status(200).json({
-        success: true
+    const token = user.getJwtToken();
+
+    res.status(201).json({
+        token
     })
 })
