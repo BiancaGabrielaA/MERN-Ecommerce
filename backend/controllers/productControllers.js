02133a679caadd5d42f7fb99d5a7d22ad22de1ff
BiancaGabrielaA <biancagabriela.asavoaei@gmail.com>
2025-07-03 10:12:29 +0300

    Create new Product and get All Products

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,5 +1,14 @@
+import Product from "../models/product.js"
+
 export const getProducts = async (req, res) => {
     res.status(200).json({
         message: "All Products"
+    })
+}
+
+export const newProduct = async (req, res) => {
+    const product = await Product.create(req.body);
+    res.status(200).json({
+        product
     })
 }
