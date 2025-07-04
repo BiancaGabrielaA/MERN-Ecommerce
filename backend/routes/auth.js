d9001b7074246635ce9435d0e33ecacaf9850439
BiancaGabrielaA <biancagabriela.asavoaei@gmail.com>
2025-07-04 09:34:20 +0300

    get user profile and update password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 import express from "express"
-import { loginUser, registerUser,logoutUser, forgotPassword, resetPassword } from "../controllers/authControllers.js";
-
+import { loginUser, registerUser,logoutUser, forgotPassword, resetPassword, getUserProfile, updatePassword } from "../controllers/authControllers.js";
+import { isAuthenticatedUser } from '../middlewares/auth.js';
 const router = express.Router();
 
 router.route("/register").post(registerUser);
@@ -9,5 +9,7 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(logoutUser);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
+router.route("/me").get(isAuthenticatedUser, getUserProfile)
+router.route("/password/update").get(isAuthenticatedUser, updatePassword)
 
 export default router;
