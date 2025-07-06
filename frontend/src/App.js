5d3100db1f9bf37d740e787d4222c14e6ce7e929
BiancaGabrielaA <biancagabriela.asavoaei@gmail.com>
2025-07-06 11:00:57 +0300

    foundation for frontend app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css';
+import Home from './components/Home';
 import Footer from './components/layout/Footer';
 import Header from './components/layout/Header';
 
 function App() {
   return (
-    <div className="App">
-        <Header />
-        <Footer />
-    </div>
+    <Router>
+      <div className="App">
+          <Header />
+          <div className="container">
+              <Routes>
+                <Route path="/" element={<Home/>} />
+              </Routes>
+          </div>
+          <Footer />
+      </div>
+    </Router>
   );
 }
 
