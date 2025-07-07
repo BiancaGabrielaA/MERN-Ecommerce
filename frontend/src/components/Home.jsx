898f1e25538c44c0e66ced0b7452a837b0ece303
BiancaGabrielaA <biancagabriela.asavoaei@gmail.com>
2025-07-07 21:17:56 +0300

    filter by price

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,13 +6,19 @@ import Loader from './layout/Loader';
 import toast from 'react-hot-toast';
 import CustomPagination from './layout/CustomPagination';
 import { useSearchParams } from 'react-router-dom';
+import Filters from './layout/Filters';
 
 const Home = () => {
 
     let [searchParams] =  useSearchParams();
     const page = searchParams.get("page") || 1;
     const keyword = searchParams.get("keyword") || 1;
-    const params = { page, keyword };
+    const min = searchParams.get("min");
+    const max = searchParams.get("max");
+    const params = { page, keyword};
+
+    min !== null && (params.min = min);
+    max !== null && (params.max = max);
 
     const { data, isLoading, error, isError} = useGetProductsQuery(params);
 
@@ -32,7 +38,7 @@ const Home = () => {
         <div className="row">
             { keyword && (
                 <div className="col-6 col-md-3 mt-5">
-                    <p>Filters</p>
+                    <Filters/>
                 </div>
             )}
             <div className={keyword ? "col-6 col-md-9" :"col-6 col-md-12" }>
